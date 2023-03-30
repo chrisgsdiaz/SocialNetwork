04abef5d2a1b9db2cbcb001053f285c64d9230eb
Christopher Guarneros Diaz <chrisgsdiaz@gmail.com>
2023-03-29 18:59:35 -0600

    - Update: actualizacion minima

--- SocialNetworkTemplate/Commons/AppDelegate.swift
+++ SocialNetworkTemplate/Commons/AppDelegate.swift
@@ -10,10 +10,23 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        /*
+        window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.makeKeyAndVisible()
+        
+        let splashVC = SplashViewController()
+        splashVC.view.frame = self.window!.frame
+        self.window?.rootViewController = UINavigationController(rootViewController: splashVC)
+        self.window?.makeKeyAndVisible()
+        */
+        let storyboard = UIStoryboard(name: "Welcome", bundle: Bundle.main)
+        let viewController = storyboard.instantiateInitialViewController()
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
         return true
     }
 
